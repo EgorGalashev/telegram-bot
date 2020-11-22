b4c5b553798698e9cf2a31ad18f710bf4338552f
EgorGalashev <EgorGalashev@76.github.com>
2020-11-22 11:40:43 +0300

    Update Games.py

--- Games.py
+++ Games.py
@@ -7,6 +7,7 @@ bot = telebot.TeleBot("1439408799:AAH6VxNNvXcrrtl3BcXUDDvnHEP3UhGBYCA")
 
 
 random_number = 0 #Число которое потом заменится на случайное
+counter = 0 #
 
 
 #######  ОТВЕТ НА КОМАНДУ START
@@ -20,19 +21,28 @@ def start_massage(message):
 ####### ОТВЕТ НА ТЕКСТОВОЕ СООБЩЕНИЕ
 @bot.message_handler(content_types = ['text'])#Ловин новые текстовые сообщения
 def otvet (message):
+    global counter # Разрешаем менять глобальную переменную counter
     try: #Попытайся сделать что-то
         user_number =int (message.text) #Превращаем ответ пользователя из строки(str) число(int)
 
-        if user_number > random_number:
+        counter += 1 # Пребавляем 1 к переменной counter
+        
+        if user_number == random_number:
+            bot.send_message(message.chat.id, "You quessed!")
+        
+        elif user_number > random_number:
             bot.send_message(message.chat.id, "Too big")
+            
         
         elif user_number < random_number:
             bot.send_message(message.chat.id, "Too small")
-        
-        else :
-            bot.send_message(message.chat.id, "You quessed!")
+
+        else:
+            bot.send_message(message.chat.id, str(random_number)+ " Я загадал это число")
+
+
     except:
-        bot.send_message(message.chat.id, "㊗ WRONG! ONLY NUMBERS!")
+        bot.send_message(message.chat, "WROOONG! ONLY NUMBERS")
 
 
 
