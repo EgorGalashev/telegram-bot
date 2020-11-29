1554c03011d99ec1cbfa0b7d0e72d1d7f4d6ba82
EgorGalashev <EgorGalashev@76.github.com>
2020-11-29 12:01:10 +0300

    1345

--- #05 Висилица.py
+++ #05 Висилица.py
@@ -5,7 +5,10 @@ from random import randint #Подключаем функцию генераци
 #Подключаемся к боту через его ТОКЕН
 bot = telebot.TeleBot("1439408799:AAH6VxNNvXcrrtl3BcXUDDvnHEP3UhGBYCA")
 
-word = "Антарктида"
+words = ["Антарктида","Параллелепипед","Пылесос","Акваланг"]
+word = words[1]
+print(word)
+
 
 #######  ОТВЕТ НА КОМАНДУ START
 @bot.message_handler(commands = ['start']) #Ловим команду START
@@ -17,8 +20,19 @@ def start_message(message):
 @bot.message_handler(content_types = ['text'])#Ловин новые текстовые сообщения
 def otvet (message):
 
-    letter = message.text
-    if letter in word:
-        bot.send_message(message.chat.id, "Такая буква есть")
+    letter = message.text.lower()
+
+    if len(letter) == 1:
+        if letter in word:
+           bot.send_message(message.chat.id, "Такая буква есть")
+
+        else:
+        bot.send_message(message.chat.id, "Нет такой буквы")
+    else:
+        if letter == word:
+            bot.send_message(message.chat.id, "Да, ты угадал!")
+        else:
+            bot.send_message(message.chat.id, "Неет, ты не угадал!")
+
 
 bot.polling() #Запрос сообщения
